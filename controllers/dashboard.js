8c7dc4f219de0777f1b8f044beddd022b80f8ebb
sahithvibudhi <v.sahithkumar@gmail.com>
2019-10-25 01:43:00 +0530

    empty state

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -8,11 +8,23 @@ exports.getDashboard = async (req, res) => {
   });
 };
 
-exports.createApp = (req, res) => {
-  const name = req.body.name;
-  const creator = req.user._id;
-  const team = req.body.team;
-  const type = req.body.type;
-  
+exports.createApp = async (req, res) => {
+  const {
+    name, creator, team, type, adapter, host, username, password,
+  } = req.body;
+  const app = new Apps();
+  app.name = name;
+  app.creator = creator;
+  app.team = team;
+  app.type = type;
+  app.adapter = adapter;
+  app.host = host;
+  app.username = username;
+  app.password = password;
+  try {
+    await app.save();
+  } catch (e) {
+    req.flash('errors', e);
+  }
   res.redirect('/dashboard');
 }
